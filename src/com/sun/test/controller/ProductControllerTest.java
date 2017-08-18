b82f92a927e3837d2d297ce962449761376c1384
Amber_Chiu <Amber_Chiu@wistron.com>
2017-08-18 13:18:59 +0800

    add ProductControllerTest

---
+++ src/com/sun/test/controller/ProductControllerTest.java
@@ -0,0 +1,22 @@
+package com.sun.test.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ProductControllerTest {
+
+	private MockMvc mockMvc;
+
+	@Test
+	public void testGetByPrice() throws Exception {
+
+	}
+
+	@Test
+	public void testSearch() throws Exception {
+
+	}
+}
