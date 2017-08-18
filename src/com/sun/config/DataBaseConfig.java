65002c7f7f329894b587488f2d9f3139fa607281
WHQ\10604307 <william_wu@wistron.com>
2017-08-18 17:08:58 +0800

    add @ImportResource

--- src/com/sun/config/DataBaseConfig.java
+++ src/com/sun/config/DataBaseConfig.java
@@ -10,56 +10,59 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@ImportResource(locations = {"file:WebContent/WEB-INF/applicationContext.xml",
+		"file:WebContent/WEB-INF/context-dispatcher.xml", "file:WebContent/WEB-INF/spring-views.xml"})
 public class DataBaseConfig {
 
 	@Bean(name = "dataSource")
-    public DataSource getDataSource() {
-        final org.apache.commons.dbcp.BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/model_practice");
-//        dataSource.setDriverClassName("com.p6spy.engine.spy.P6SpyDriver");
-//        dataSource.setUrl("jdbc:p6spy:mysql://localhost:3306/wrel");
-        dataSource.setUsername("root");
-        dataSource.setPassword("123456q");
+	public DataSource getDataSource() {
+		final org.apache.commons.dbcp.BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/model_practice");
+		// dataSource.setDriverClassName("com.p6spy.engine.spy.P6SpyDriver");
+		// dataSource.setUrl("jdbc:p6spy:mysql://localhost:3306/wrel");
+		dataSource.setUsername("root");
+		dataSource.setPassword("");
 
-        return dataSource;
-    }
+		return dataSource;
+	}
 
-    @Autowired
-    @Bean(name = "sessionFactory")
-    public SessionFactory getSessionFactory(DataSource dataSource) {
+	@Autowired
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
 
-        final LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
-        sessionBuilder.scanPackages("com.sun.entity");
-        sessionBuilder.addProperties(this.getHibernateProperties());
+		final LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		sessionBuilder.scanPackages("com.sun.entity");
+		sessionBuilder.addProperties(this.getHibernateProperties());
 
-        return sessionBuilder.buildSessionFactory();
-    }
+		return sessionBuilder.buildSessionFactory();
+	}
 
-    @Autowired
-    @Bean(name = "transactionManager")
-    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
-        final HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+	@Autowired
+	@Bean(name = "transactionManager")
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		final HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
 
-        return transactionManager;
-    }
-    //####################################################################
-    //## [Method] sub-block : 
+		return transactionManager;
+	}
+	// ####################################################################
+	// ## [Method] sub-block :
 
-    private Properties getHibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.show_sql", "false");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-        return properties;
-    }
-    //####################################################################
-    //====
-    //== [Method] Block Stop 
-    //================================================
+	private Properties getHibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.show_sql", "false");
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		return properties;
+	}
+	// ####################################################################
+	// ====
+	// == [Method] Block Stop
+	// ================================================
 }
